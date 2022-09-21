eddcb3249e38dc3b61f2416e9772cec71cfdf2ae
unknown <harshs381@gmail.com>
2022-09-21 13:56:31 +0530

    code changed by harsh

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,17 @@
 // Q. Check if given string is palindrome or not.
 
 
-let str ="vikas";
+let string="vikas";
+
+let bag="";
+
+for(i=string.length-1;i>=0;i--){
+    bag=bag+string[i];
+}
+
+if(bag===string){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
